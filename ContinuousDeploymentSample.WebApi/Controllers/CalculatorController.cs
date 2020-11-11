7a5ce71f33e14bde3b0a2ff2b31c8d195b601373
madiron <info@luca.evaroni.name>
2020-11-11 21:48:38 +0100

    Calculator Controller

---
+++ ContinuousDeploymentSample.WebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContinuousDeploymentSample.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("sum")]
+        public IActionResult Sum(int a, int b)
+        {
+            var sum = Calculator.Sum(a, b);
+
+            return Ok($"Sum is {sum}");
+        }
+    }
+
+    public class Calculator
+    {
+        public static int Sum(int a, int b) { return a + b; }
+    }
+}
